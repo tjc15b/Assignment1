d0e756ad66d426b0929c20e15de044d7807f5a34
tjc15b <tjc15b@my.fsu.edu>
2017-10-09 20:18:20 -0400

    Add include for strong hammer class

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -11,6 +11,7 @@
 #include "CommonSpear.h"
 #include "SimpleAxe.h"
 #include "CrazyRandomSword.h"
+#include "StrongHammer.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
@@ -36,6 +37,10 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
 
     if(name.compare("random") == 0) {
         return new CrazyRandomSword();
+    }
+
+    if(name.compare("hammer") == 0) {
+        return new StrongHammer();
     }
 
     throw "Invalid weapon";
