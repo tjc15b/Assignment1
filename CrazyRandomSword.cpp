7d89911cd6c379def40fe48eda6cbb255274313a
tjc15b <tjc15b@my.fsu.edu>
2017-10-09 22:07:07 -0400

    Add check for if damage is negative

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -7,5 +7,8 @@ double CrazyRandomSword::hit(double armor){
     double randnum = fmod(rand(),((armor/2) + 1));
     damage += randnum;
 
+    if(damage < 0)
+      return 0;
+
     return damage;
 }
